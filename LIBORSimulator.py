3d2e33ca488893871a9e855363c5987432e98638
Prasannavathan DK <prasannavathan.duraisamy-kannan@uni-ulm.de>
2024-05-24 00:15:33 +0200

    Shared Memory removed

--- LIBORSimulator.py
+++ LIBORSimulator.py
@@ -14,7 +14,8 @@ class LIBORSim(EulerScheme):
             model=SM.SpotMeasure(type = type, maturity=maturity, prices=prices, scale=scale)
         super().__init__(model=model, iter=iter)
         self._sm = np.zeros((self.iter, len(model.maturityGrid)-1, len(model.timeGrid))) #depth=iteration, row=maturity, column=discretizedTime
-        self._ran = model.distribution()(self._sm.shape) 
+        self._ran = model.distribution()(self._sm.shape)
+        print("Pre-Processing done!") 
 
     @property 
     def matrix(self):
@@ -59,35 +60,34 @@ class LIBORSim(EulerScheme):
         #print(maturityIndex)
         return ((self.model.bondPrices[maturityIndex]-self.model.bondPrices[maturityIndex+1])/((self.model.maturityGrid[maturityIndex+1]-self.model.maturityGrid[maturityIndex])*self.model.bondPrices[maturityIndex+1]))
 
-    def subEngine(self, iter):
-        for j in range(self.matrix.shape[1]):
-            start = iter*self.matrix.shape[1]*self.matrix.shape[2] + j*self.matrix.shape[2]
-            Solver.SamplePath(iter, j, start, self.model.SDE, self.model.timeGrid, self.sharedRandom, self.sharedMatrix)
-
+    def subEngine(self, i):
+        print("Processing iteration:", i)
+        return [(i, j, Solver.SamplePath(iter=i, row=j, start=0, SDE=self.model.SDE, timeGrid=self.model.timeGrid, random=self.random[i,j], matrix=self.matrix[i,j])) for j in range(self.matrix.shape[1])]
+    
     def engine(self):
         if Solver.parallel is not True:
            for i in range(self.matrix.shape[0]):
+                print("Processing iteration:", i)
                 for j in range(self.matrix.shape[1]):
                     Solver.SamplePath(iter=i, row=j, start=0, SDE=self.model.SDE, timeGrid=self.model.timeGrid, random=self.random[i,j], matrix=self.matrix[i,j])
-        else:   
-            self.sharedMatrix = Solver.manager.list(self.matrix.flatten())
-            self.sharedRandom = Solver.manager.list(self.random.flatten())
-            results = [Solver.threadPool.apply_async(func = self.subEngine, args=(i,)) for i in range(self.matrix.shape[0])]
-            # Wait for all results to be completed
-            for result in results:
-                result.wait()     
-            self.matrix = np.array(self.sharedMatrix).reshape(self.matrix.shape)         
+        else:
+            asyncRes = [Solver.threadPool.apply_async(func = self.subEngine, args=(i,)) for i in range(self.matrix.shape[0])]
+            for ares in asyncRes:
+                for elem in ares.get():
+                    self.matrix[elem[0], elem[1]] = elem[2]      
         return  
     
     def simulate(self):
         print(self.matrix.shape)
         self.matrix[...,0] = [self.initCondition(T) for T in range(len(self.model.maturityGrid)-1)]
         self.execute()
+        print("Processing done!") 
 
      #Summary from the sample paths
     def processSP(self):
         #print(self.matrix)
         hp.plotSP(self.matrix[0])
         hp.plot()
+        print("Post-Processing done!") 
         return self.matrix
         
--- main.py
+++ main.py
@@ -6,17 +6,18 @@ from LIBORSimulator import LIBORSim
 import NumericalSolver  
 
 def main():
-    print("LIBOR Market Model- Simulation")
+    print("LIBOR Market Model")
+    print("Simulation started...")
     timer = hp.timer()
     timer.start()
     maturityDates = [1,2,3,4,5,6,7,8,9,10]
     bondPrices = [10,9,8,7,6,5,4,3,2,1]
-    simulator = LIBORSim(maturity=maturityDates, prices=bondPrices, measure=0, iter = 50, scale = 12, type=0)
-    simulator.simulate()
+    simulator = LIBORSim(maturity=maturityDates, prices=bondPrices, measure=0, iter = 20, scale = 12, type=0)
+    simulator.simulate() 
     timer.stop()
     simulator.processSP()  
     timer.stop()  
-    print("Simulation complete")
+    print("Simulation complete :)")
 
 if __name__ == "__main__":
     with multiprocessing.Manager() as manager:

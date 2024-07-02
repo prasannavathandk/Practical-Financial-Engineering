7964f69a15f50931c49f383a881276cfaf048827
Niklas Knecht <knecht.n@gmx.de>
2024-07-02 20:50:25 +0200

    More stuff Swap stuff

--- PricingEngine.py
+++ PricingEngine.py
@@ -77,27 +77,44 @@ class PricingEngine:
         else:
             return 0
         
-    def Swaption(self, T: float, timepoint: float, StartDate: float, EndDate: float, Nominal: float, Strike: float, FloatingRate: float, Frequency: float):
+
+    def VanillaSwap(self, T: float,  StartDate: float, EndDate: float, Nominal: float, FixedRate: float, FloatingRate: float, Frequency: float, iteration: int):
+
+        # Check if Intervalls alling
+        if self.intervals[0] != 1 / Frequency:
+            raise ValueError("Intervalls not aligned with Frequency")
+        
+        # Calculate fixed leg cash flows
+        fixed_cash_flows = [FixedRate * Nominal] * (EndDate - StartDate)*Frequency
+
+        # Calculate the floatiing 
+        discount_factors = [1.0]
+        mask = (self.simulation.epoch == iteration) & (self.simulation.Time == T)
+        discount_curve = self.simulation[mask].iloc[:, 2:].values
+
+        for i in range(StartDate, EndDate):
+            discount_factors.append(discount_factors[-1] * 1 / (1 + discount_curve[i-1] * self.intervals[i-1]))
+
+        # Present value of the floating leg (assumed to be equal to notional value)
+        pv_floating_leg = discount_factors[0] - discount_factors[-1]
+        
+        # Calculate the sum of discount factors for the periods
+        sum_discount_factors = sum(discount_factors[i] for i in range(1, len(discount_factors)))
+    
+        return pv_floating_leg - sum_discount_factors
+    
+    
+    def Swaption(self, T: float, timepoint: float, StartDate: float, EndDate: float, Nominal: float, Strike: float, FloatingRate: float, Frequency: float, iteration: int):
 
         # Check if Intervalls alling
         if self.intervals[0] != 1 / Frequency:
             raise ValueError("Intervalls not aligned with Frequency")
         
         if T == timepoint:
-            return 
+            swapPrice = self.VanillaSwap(T, StartDate, EndDate, Nominal, FixedRate=Strike, FloatingRate=FloatingRate, Frequency=Frequency, iteration=iteration)
+            return swapPrice
         else:
             return 0
-        
-
-    def VanillaSwap(self, T: float,  StartDate: float, EndDate: float, Nominal: float, FixedRate: float, FloatingRate: float, Frequency: float):
-
-        # Check if Intervalls alling
-        if self.intervals[0] != 1 / Frequency:
-            raise ValueError("Intervalls not aligned with Frequency")
-        
-         # Calculate fixed leg cash flows
-        fixed_cash_flows = [FixedRate * Nominal] * (EndDate - StartDate)*Frequency
-        
 
     def SwapRate(self, StartDate: float, EndDate: float, Frequency: float):
 

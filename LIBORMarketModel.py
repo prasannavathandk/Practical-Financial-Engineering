b40225f3dad34f0e5754c519f4558d16fc3ff026
Prasannavathan DK <prasannavathan.duraisamy-kannan@uni-ulm.de>
2024-07-09 23:24:50 +0200

    Simulation- Piecewise Volatility

--- LIBORMarketModel.py
+++ LIBORMarketModel.py
@@ -71,7 +71,8 @@ class LIBORModel(ModelInterface):
 
     def sigma(self, t, n):
         #print("LIBORModel.sigma", t, n)
-        return (self.timeGrid[t+1])*(self.volatility[n, int(math.floor(self.timeGrid[t]))])
+        t_ = self.timeGrid[t+1] - math.floor(self.timeGrid[t+1])
+        return (1)*(self.volatility[n, int(math.floor(self.timeGrid[t]))])
                 
     #Implementation of general drift
     @abstractmethod

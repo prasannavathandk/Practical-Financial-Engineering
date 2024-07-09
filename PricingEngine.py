d3973dafcc03a996a7ffa1674b989731dffaf2a5
Niklas Knecht <knecht.n@gmx.de>
2024-07-09 18:53:09 +0200

    Added the volcalibration

--- PricingEngine.py
+++ PricingEngine.py
@@ -1,5 +1,30 @@
 import numpy as np
+import pandas as pd
 import math 
+
+def volCalibration(capletVolatility):
+
+    # construct output object
+    M = len(capletVolatility)
+    volMatrix = np.zeros((M, M))
+    period_volatility = []
+
+    for i in range(M):
+
+        if i == 0: # One period vol can be backed out from one period caplet
+            cv = capletVolatility[i]
+            period_volatility.append(cv)
+
+        else: # Iteratively solve the equations for n-period vol 
+            cv = capletVolatility[i]
+            T_i = i+1
+            cvs = (np.array(period_volatility)**2)
+            
+            new_cv = (T_i*(cv**2) - sum(cvs))**(0.5)
+            period_volatility.append(new_cv)
+
+        print(period_volatility)
+        volMatrix[i, :len
 
 
 class PricingEngine:
@@ -38,10 +63,12 @@ class PricingEngine:
                 pricing_parameter['timepoint'] = round(pricing_parameter['timepoint'], time_precision)
 
                 CashFlow = self.PricingMethods()[asset](**pricing_parameter)
+          
         
                 Price = Price + DiscountFactor * CashFlow
+                print(Price)
             Prices.append(Price)
-            print('Pricing run for iteration:', k, ' finished with price:', Price)
+            #print('Pricing run for iteration:', k, ' finished with price:', Price)
 
         Price = np.array(Prices).mean()
 
@@ -91,18 +118,25 @@ class PricingEngine:
         # Calculate the floatiing 
         discount_factors = [1.0]
         mask = (self.simulation.epoch == iteration) & (self.simulation.Time == T)
-        discount_curve = self.simulation[mask].iloc[:, 2:].values
+        discount_curve = self.simulation[mask].iloc[:, 2:].values[0]
+        new_discount_curve = []
+        #print(discount_curve)
+        for i in range(StartDate+1, EndDate+1):
+            new_discount_curve.append(discount_curve[i])
+            discount_factors.append(discount_factors[-1]  / (1 + discount_curve[i-1] * self.intervals[i-1]))
 
-        for i in range(StartDate, EndDate):
-            discount_factors.append(discount_factors[-1] * 1 / (1 + discount_curve[i-1] * self.intervals[i-1]))
+        discount_curve = discount_curve[1:]
 
         # Present value of the floating leg (assumed to be equal to notional value)
-        pv_floating_leg = discount_factors[0] - discount_factors[-1]
+        pv_floating_leg = sum(discount_factors[i] * discount_curve[i] * Nominal  for i in range(1, len(discount_factors)))
         
         # Calculate the sum of discount factors for the periods
-        sum_discount_factors = sum(discount_factors[i] for i in range(1, len(discount_factors)))
+        pv_fixed_leg = sum(discount_factors[i] * FixedRate * Nominal  for i in range(1, len(discount_factors)))
+
+        #print(pv_floating_leg)
+        #print(pv_fixed_leg)
     
-        return pv_floating_leg - sum_discount_factors
+        return pv_floating_leg - pv_fixed_leg
     
 
     def Swaption(self, T: float, timepoint: float, StartDate: float, EndDate: float, Nominal: float, Strike: float, Frequency: float, iteration: int):
@@ -113,6 +147,8 @@ class PricingEngine:
         
         if T == timepoint:
             swapPrice = self.VanillaSwap(T, StartDate, EndDate, Nominal, FixedRate=Strike, Frequency=Frequency, iteration=iteration)
+            if swapPrice < 0:
+                swapPrice = 0
             return swapPrice
         else:
             return 0

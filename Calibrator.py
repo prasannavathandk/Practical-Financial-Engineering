cfb23d1711924c475d942b229dac11f32888c0da
Niklas Knecht <knecht.n@gmx.de>
2024-07-10 15:49:47 +0200

    who the fuck uses percent instead of decimal?

--- Calibrator.py
+++ Calibrator.py
@@ -65,7 +65,8 @@ class Calibrator:
         bondPrices = (1 / (np.array(forwardCurve) + 1).cumprod()).tolist()
 
         for i in range(M):
-            caplet_prices = [hp.BC(F=forwardCurve[i], K=forwardCurve[i], T=(i+1), b=bondPrices[i], sigma=capletVolatility[i]) for i in range(M)]
+            tmp_cv = np.array(capletVolatility) / 100
+            caplet_prices = [hp.BC(F=forwardCurve[i], K=forwardCurve[i], T=(i+1), b=bondPrices[i], sigma=tmp_cv[i]) for i in range(M)]
 
 
         # Calibrate the volatility 
